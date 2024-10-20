b4e46406a9471c77be965246ef781fd43146de58
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-10-20 11:06:34 +0300

    feat:integrated image

--- src/app/components/pages/home/integrated/Integrated.jsx
+++ src/app/components/pages/home/integrated/Integrated.jsx
@@ -1,13 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Integrated.module.scss';
 import Image from 'next/image';
-import viber from './images/viber.png';
-import mail from './images/mail.png';
-import avito from './images/avito.png';
-import message from './images/message.png';
-import sms from './images/sms.png';
-import instagram from './images/Instagram.png';
-import telegram from './images/telegram.png';
+import { data } from './data';
 
 const cx = classNames.bind(styles);
 
@@ -20,83 +14,19 @@ const Integrated = () => {
         </h2>
         <div className={cx('list-wrapper')}>
           <ul className={cx('list', 'carousel-track')}>
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={viber}
-                  className={cx('image')}
-                  alt="WhatsApp"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>WhatsApp</span>
-            </li>
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={mail}
-                  className={cx('image')}
-                  alt="viber"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>Почта</span>
-            </li>{' '}
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={avito}
-                  className={cx('image')}
-                  alt="Avito"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>Avito</span>
-            </li>
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={message}
-                  className={cx('image')}
-                  alt="message"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>Messenger</span>
-            </li>{' '}
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={sms}
-                  className={cx('image')}
-                  alt="sms"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>SMS</span>
-            </li>{' '}
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={instagram}
-                  className={cx('image')}
-                  alt="instagram"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>Instagram*</span>
-            </li>{' '}
-            <li className={cx('item')}>
-              <div className={cx('image-wrapper')}>
-                <Image
-                  src={telegram}
-                  className={cx('image')}
-                  alt="Telegram"
-                  loading="lazy"
-                />
-              </div>
-              <span className={cx('text')}>Telegram</span>
-            </li>
+            {data.map((item, index) => (
+              <li key={index} className={cx('item')}>
+                <div className={cx('image-wrapper')}>
+                  <Image
+                    src={item.image}
+                    className={cx('image')}
+                    alt={item.text}
+                    loading="lazy"
+                  />
+                </div>
+                <span className={cx('text')}>{item.text}</span>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -105,3 +35,70 @@ const Integrated = () => {
 };
 
 export default Integrated;
+
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={mail}
+//     className={cx('image')}
+//     alt="viber"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>Почта</span>
+// </li>
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={avito}
+//     className={cx('image')}
+//     alt="Avito"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>Avito</span>
+// </li>
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={message}
+//     className={cx('image')}
+//     alt="message"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>Messenger</span>
+// </li>{' '}
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={sms}
+//     className={cx('image')}
+//     alt="sms"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>SMS</span>
+// </li>{' '}
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={instagram}
+//     className={cx('image')}
+//     alt="instagram"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>Instagram*</span>
+// </li>{' '}
+// <li className={cx('item')}>
+// <div className={cx('image-wrapper')}>
+//   <Image
+//     src={telegram}
+//     className={cx('image')}
+//     alt="Telegram"
+//     loading="lazy"
+//   />
+// </div>
+// <span className={cx('text')}>Telegram</span>
+// </li>
---
+++ src/app/components/pages/home/integrated/data.js
@@ -0,0 +1,94 @@
+import WhatsApp from './images/whatsApp.png';
+import mail from './images/mail.png';
+import avito from './images/avito.png';
+import message from './images/message.png';
+import sms from './images/sms.png';
+import instagram from './images/Instagram.png';
+import telegram from './images/telegram.png';
+
+export const data = [
+  {
+    image: WhatsApp,
+    text: 'WhatsApp',
+  },
+  {
+    image: mail,
+    text: 'Почта',
+  },
+  {
+    image: avito,
+    text: 'Avito',
+  },
+  {
+    image: message,
+    text: 'Messenger',
+  },
+  {
+    image: sms,
+    text: 'SMS',
+  },
+  {
+    image: instagram,
+    text: 'Instagram*',
+  },
+  {
+    image: telegram,
+    text: 'Telegram',
+  },
+  {
+    image: WhatsApp,
+    text: 'WhatsApp',
+  },
+  {
+    image: mail,
+    text: 'Почта',
+  },
+  {
+    image: avito,
+    text: 'Avito',
+  },
+  {
+    image: message,
+    text: 'Messenger',
+  },
+  {
+    image: sms,
+    text: 'SMS',
+  },
+  {
+    image: instagram,
+    text: 'Instagram*',
+  },
+  {
+    image: telegram,
+    text: 'Telegram',
+  },
+  {
+    image: WhatsApp,
+    text: 'WhatsApp',
+  },
+  {
+    image: mail,
+    text: 'Почта',
+  },
+  {
+    image: avito,
+    text: 'Avito',
+  },
+  {
+    image: message,
+    text: 'Messenger',
+  },
+  {
+    image: sms,
+    text: 'SMS',
+  },
+  {
+    image: instagram,
+    text: 'Instagram*',
+  },
+  {
+    image: telegram,
+    text: 'Telegram',
+  },
+];
